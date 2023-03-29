f7f8a280cd6b6323ea41418f40c7d72d24a0aaf0
mackeriv <mackeriv@gmail.com>
2023-03-29 13:22:00 -0300

    Third day.

---
+++ src/main/java/org/example/ExtratorNASA.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ExtratorNASA {
+
+    public List<Conteudo> extraiConteudos(String json) {
+
+        //extrair só os dados que interessam (titulo, poster, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeAtributos = parser.parse(json);
+
+        List<Conteudo> conteudos = new ArrayList<>();
+
+        for (Map<String, String> atributos : listaDeAtributos) {
+            String titulo = atributos.get("title");
+            String urlImagem = atributos.get("url");
+            var conteudo = new Conteudo(titulo, urlImagem);
+
+            conteudos.add(conteudo);
+        }
+
+        return conteudos;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,45 +3,38 @@ package org.example;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        //fazer uma conexão HTTP e pegar os top 250 filmes
+        //fazer uma conexão HTTP e pegar os conteudos
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularTVs.json";
 
-        var client = HttpClient.newHttpClient();
-        URI endereco = URI.create(url);
-
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        String body = response.body();
+        var http = new ClienteHTTP();
+        String json = http.buscaDados(url);
 
         //extrair só os dados que interessam (titulo, poster, classificação)
         var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        List<Map<String, String>> listaDeConteudos = parser.parse(json);
 
         //exibir e manipular os dados
         var geradora = new GeradoraDeFigurinhas();
 
-        for (Map<String, String> filme: listaDeFilmes) {
-            String urlImagem = filme.get("image");
+        for (Map<String, String> conteudo: listaDeConteudos) {
+            String urlImagem = conteudo.get("image");
             URI uri = new URI(urlImagem);
             URL novoUrl = uri.toURL();
 
             InputStream inputStream = novoUrl.openStream();
 
-            String titulo = filme.get("title");
+            String titulo = conteudo.get("title");
             String nomeArquivo = "saida/" + titulo + ".png";
 
             geradora.cria(inputStream, nomeArquivo);
 
-            System.out.println("Título: " + "\u001b[1m" + filme.get("title") + "\u001b[m");
+            System.out.println("Título: " + "\u001b[1m" + conteudo.get("title") + "\u001b[m");
 
             System.out.println("\n");
 
